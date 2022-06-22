d9a9888fabfdbba2bd4814cd3f1b2c5da8aef099
Juan Cassiano <juancassiano@hotmail.com>
2022-06-22 17:04:18 -0300

    Import query from grapcms

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,21 @@
+import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "./Lesson";
 
+const GET_LESSON_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      slug
+      title
+      availableAt
+      lessonType
+    }
+  }
+`;
+
 export function Sidebar() {
+  const { data } = useQuery(GET_LESSON_QUERY);
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
