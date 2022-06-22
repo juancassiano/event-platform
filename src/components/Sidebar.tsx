d8f7789ebbf6ef3901f10f3d0f242dd145f7bfb6
Juan Cassiano <juancassiano@hotmail.com>
2022-06-22 17:39:06 -0300

    Use date-fns to format date lesson

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,7 +14,7 @@ const GET_LESSON_QUERY = gql`
 `;
 
 interface GetLessonsQueryResponse {
-  Lessons: {
+  lessons: {
     id: string;
     title: string;
     slug: string;
@@ -32,12 +32,17 @@ export function Sidebar() {
         Cronograma de Aulas
       </span>
       <div className="flex flex-col gap-8">
-        <Lesson
-          title="Aula 01"
-          slug="aula-01"
-          availableAt={new Date()}
-          type="class"
-        />
+        {data?.lessons.map((lesson) => {
+          return (
+            <Lesson
+              key={lesson.id}
+              title={lesson.title}
+              slug={lesson.slug}
+              availableAt={new Date(lesson.availableAt)}
+              type={lesson.lessonType}
+            />
+          );
+        })}
       </div>
     </aside>
   );
