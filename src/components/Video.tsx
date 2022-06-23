542d46f7f24fb4ac532b97129a4d83eada04c70b
Juan Cassiano <juancassiano@hotmail.com>
2022-06-23 00:41:48 -0300

    vimejs install for use youtube videos in page

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,20 @@
-import { DiscordLogo, Lightning } from "phosphor-react";
+import { Player, Youtube } from "@vime/react";
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Lightning,
+} from "phosphor-react";
 
 export function Video() {
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w[1100px] max-h[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w[1100px] max-h[60vh] aspect-video">
+          <Player>
+            <Youtube />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w[1100px] mx-auto">
@@ -14,13 +24,30 @@ export function Video() {
               Aula 01 - Abertura do Ignite Lab
             </h1>
 
-            <p className="mt-4 text-gray-200">
+            <p className="mt-4 text-gray-200 leading-relaxed">
               Nessa aula vamos dar início ao projeto criando a estrutura base da
               aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também
               realizar o setup do nosso projeto no GraphCMS criando as entidades
               da aplicação e integrando a API GraphQL gerada pela plataforma no
               nosso front-end utilizando Apollo Client.
             </p>
+
+            <div className="flex items-center gap-4 mt-6">
+              <img
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+                src="http://github.com/juancassiano.png"
+                alt=""
+              />
+            </div>
+
+            <div className="leading-relaxed">
+              <strong className="font-bold text-2xl block">
+                Juan Cassiano
+              </strong>
+              <span className="text-gray-200 text-sm block">
+                Backend Developer
+              </span>
+            </div>
           </div>
 
           <div className="flex flex-col gap-4">
@@ -41,7 +68,43 @@ export function Video() {
             </a>
           </div>
         </div>
-        <div></div>
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Material Complementar</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelerar seu desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight />
+            </div>
+          </a>
+
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Wallpapers Exclusivos</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Baixe Wallpapers exclusivos
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight />
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
